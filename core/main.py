79db8cc7e5ac61cbbaf0c0fd660c297fc76719a0
MH Farahmand ツ <mhfmobin@gmail.com>
2024-07-22 15:49:44 +0330

    Not competitively priced

--- core/main.py
+++ core/main.py
@@ -185,8 +185,8 @@ def get_amazon_price(url, max_retries=3, backoff_factor=0.3):
 
         # check if the product is out of stock
 
-        
-        if soup.find(string='Currently unavailable.') or soup.find(string='No featured offers available'):
+         
+        if soup.find(string='Currently unavailable.') or soup.find(string='No featured offers available') or soup.find(string='Not competitively priced'):
             return {"error": "unavailable", "price": None, "title": title}
         
         price = None
