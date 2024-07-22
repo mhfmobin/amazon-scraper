318f97c0af9d19397444aa2d99eb01773a30d3cd
MH Farahmand ツ <mhfmobin@gmail.com>
2024-07-22 11:18:22 +0330

    api endpoint

--- validateLink.php
+++ validateLink.php
@@ -1,15 +1,61 @@
 <?php
+header('Content-Type: application/json');
 
-$url = "https://www.amazon.ae/Samsung-Crystal-Processor-Airslim-Dynamic/dp/B0C1ZDWTBF/ref=sr_1_2?_encoding=UTF8&content-id=amzn1.sym.86fde58f-9eb9-4690-b6d2-5ed3baac4368&dib=eyJ2IjoiMSJ9.faf2VP_ltM3srXsaGi_dFmPF4usM_S5iJbqswWFNAMfwbFilhatBsOfyDwiC4lH_V7qZyjsl-Ql0IOJlmpFH1_TlxPez5h8kOElHWXEaRxcyTW1vcsf2nIjaCUGpW15YRoTIv6WVmTwIl5tW7rIDw7F9M-TTa75q8JD3yKURlokx3_PqyR-ORjvIoL6nX46NIaBjpnuaHdkMdjmep2tSO6qPryJNYwZk_O-tbRxDOpdexCihD_7TypWCGOftH9yyPVIT4P9OI6O4t1NQw5WlER3LAebOXPZikvpLF6kq-vI.ZA2IuWsOhFqY8w2AdT1_LWIt0R0HelEyQQFETg2aLdA&dib_tag=se&pd_rd_r=cc7d1392-cd76-4c46-803c-ffd03d397292&pd_rd_w=hMPjj&pd_rd_wg=fK53M&pf_rd_p=86fde58f-9eb9-4690-b6d2-5ed3baac4368&pf_rd_r=6PZWBAB6T414DRMG8F5K&qid=1721632869&refinements=p_85%3A15602504031&rps=1&s=electronics&sr=1-2&th=1";
+$allowed_ips = [
+    '127.0.0.1',  // localhost
+    '::1',        // localhost IPv6
+];
+
+$client_ip = $_SERVER['REMOTE_ADDR'];
+if (!in_array($client_ip, $allowed_ips)) {
+    http_response_code(403);
+    echo json_encode(['error' => 'Access denied']);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+    exit;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+$url = $input['url'] ?? '';
+
+if (empty($url)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'URL is required']);
+    exit;
+}
 
 $result = validateAndShortenAmazonLink($url);
 
 if ($result['isValid']) {
-    $command = "python3 main.py " . escapeshellarg($result['shortUrl']); 
+    $command = "python3 main.py " . escapeshellarg($result['shortUrl']);
     $output = shell_exec($command);
-    $result = json_decode($output, true);
-}
+    $pythonResult = json_decode($output, true);
 
+    if ($pythonResult === null) {
+        echo json_encode(['error' => 'Error processing the URL']);
+    } else {
+        echo json_encode([
+            'valid' => true,
+            'originalUrl' => $result['originalUrl'],
+            'shortUrl' => $result['shortUrl'],
+            'asin' => $result['asin'],
+            'error' => $pythonResult['error'],
+            'price' => $pythonResult['price'],
+            'title' => $pythonResult['title'],
+            'elapsedTime' => $pythonResult['elapsed_time']
+        ]);
+    }
+} else {
+    echo json_encode([
+        'valid' => false,
+        'originalUrl' => $result['originalUrl'],
+        'error' => 'Invalid Amazon.ae URL'
+    ]);
+}
 
 function validateAndShortenAmazonLink($url) {
     // General pattern for Amazon URLs
